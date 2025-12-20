f6bee4c4a458f2cb02fe71afd3bf803061d51731
Cosmas Onyekwelu <dpositiveworld@gmail.com>
2025-12-20 23:27:49 +0000

    Phase 1 complete: Trade Recording System (Core of the project).

--- otcbook_server/settings.py
+++ otcbook_server/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Local apps
-    "users",
+    "users", "trades",
 
     # Third-party apps
     "rest_framework",
